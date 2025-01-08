0383f3803e929b0ee6cdf5dff0001f6d8abd2fa1
Samuel <83781440+samuelmarc@users.noreply.github.com>
2025-01-08 20:10:05 -0300

    Adding default timeout

--- pyromod/config/__init__.py
+++ pyromod/config/__init__.py
@@ -3,10 +3,10 @@ from types import SimpleNamespace
 config = SimpleNamespace(
     timeout_handler=None,
     stopped_handler=None,
+    default_timeout=None,
     throw_exceptions=True,
     unallowed_click_alert=True,
-    unallowed_click_alert_text=("[pyromod] You're not expected to click this button."),
-    disable_startup_logs=False,
+    unallowed_click_alert_text=("[pyromod] You're not expected to click this button.")
 )
 
 __all__ = ["config"]
